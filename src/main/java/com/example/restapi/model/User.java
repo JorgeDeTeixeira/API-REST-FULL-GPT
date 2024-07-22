c78200c2075e71e35890d35bb7b8c257a0a13ce9
Jorge Teixeira <jorgedeteixeirademelo@gmail.com>
2024-07-21 22:42:19 -0300

    adição do get e set do Id

--- src/main/java/com/example/restapi/model/User.java
+++ src/main/java/com/example/restapi/model/User.java
@@ -13,6 +13,14 @@ public class User {
 	private String name;
 	private String email;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public String getName() {
 		return name;
 	}
